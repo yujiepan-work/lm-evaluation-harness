2f9a6114b55f7c4a5b57a10c9f678887af19a297
lintangsutawika <lintang@sutawika.com>
2023-03-09 16:47:10 +0000

    can load configurable tasks

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from typing import List, Union
 
-from lm_eval.api.task import Task
+from lm_eval.api.task import Task, ConfigurableTask
 
 from . import lambada
 
@@ -43,16 +43,29 @@ def get_task_name_from_object(task_object):
     )
 
 
-def get_task_dict(task_name_list: List[Union[str, Task]]):
+def get_task_name_from_config(task_config):
+    return "configurable_{dataset_path}_{dataset_name}".format(**task_config)
+
+
+def get_task_dict(task_name_list: List[Union[str, dict, Task]]):
     task_name_dict = {
         task_name: get_task(task_name)(config={"num_fewshot": 0}) # TODO: don't hardcode this and figure out a proper config system
         for task_name in task_name_list
         if isinstance(task_name, str)
     }
+    task_name_from_config_dict = {
+        get_task_name_from_config(task_config): ConfigurableTask(config=task_config)
+        for task_config in task_name_list
+        if isinstance(task_config, dict)
+    }
     task_name_from_object_dict = {
         get_task_name_from_object(task_object): task_object
         for task_object in task_name_list
-        if not isinstance(task_object, str)
+        if isinstance(task_object, Task)
     }
     assert set(task_name_dict.keys()).isdisjoint(set(task_name_from_object_dict.keys()))
-    return {**task_name_dict, **task_name_from_object_dict}
+    return {
+        **task_name_dict,
+        **task_name_from_config_dict,
+        **task_name_from_object_dict
+    }
