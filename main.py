19459e7ff1a6ecb99b54fac3fa3261e585e3e4e5
lintangsutawika <lintang@sutawika.com>
2023-03-27 14:09:34 +0000

    batch size default 1

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def parse_args():
     parser.add_argument("--config", default=None)
     parser.add_argument("--provide_description", action="store_true")
     parser.add_argument("--num_fewshot", type=int, default=0)
-    parser.add_argument("--batch_size", type=int, default=None)
+    parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--device", type=str, default=None)
     parser.add_argument("--output_path", default=None)
     parser.add_argument("--limit", type=int, default=None)
